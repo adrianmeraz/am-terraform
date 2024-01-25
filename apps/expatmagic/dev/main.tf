d10174cd987476d90b7e367e128df19abb0c748b
Adrian Meraz <adrian@rydeas.com>
2024-01-25 18:02:59 -0500

    cleaned up variables now plan succeeds

--- apps/expatmagic/dev/main.tf
+++ apps/expatmagic/dev/main.tf
@@ -7,24 +7,25 @@ locals {
 module  "ecr" {
   source = "../../../modules/ecr"
 
-  name = local.app_name
+  app_name = local.app_name
   environment = local.environment
+  name = local.app_name
 }
 
 module "lambda_role" {
   source = "../../../modules/lambda_role"
 
   app_name = local.app_name
-  name = local.app_name
   environment = local.environment
+  name = local.app_name
 }
 
 module "lambda_iam_policy" {
   source = "../../../modules/lambda_iam_policy"
 
   app_name = local.app_name
-  name = local.app_name
   environment = local.environment
+  name = local.app_name
 
   path = "/"
   description = "AWS IAM Policy for managing aws lambda role"
@@ -35,23 +36,12 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
   policy_arn  = module.lambda_iam_policy.arn
 }
 
-resource "aws_lambda_function" "lambda_function" {
-  app_name                       = local.app_name
-  filename                       = "${path.module}/python/hello-python.zip"
-
-  function_name                  = "Spacelift_Test_Lambda_Function"
-  role                           = module.lambda_iam_policy.arn
-  handler                        = "index.lambda_handler"
-  runtime                        = "python3.8"
-  depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
-}
-
 module "lambda_function" {
   source = "../../../modules/lambda_function"
   app_name = local.app_name
   environment = local.environment
 
-  function_name = "Expat Magic lambda function"
+  function_name = local.app_name
   handler = "com.dn.StreamLambdaHandler"
   image_uri = module.ecr.repository_url
   role = module.lambda_iam_policy.arn
