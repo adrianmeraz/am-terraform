d0886eb754074a08e0110ed9bdc92c7b92526929
Adrian Meraz <adrian@rydeas.com>
2024-01-25 18:57:07 -0500

    cleaned up var defaults

--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -8,13 +8,13 @@ variable "environment" {
   type        = string
 }
 
-variable "bucket" {
-  description = "Name of s3 bucket"
-  type        = string
-}
-
 variable "tags" {
   description = "A map of tags to add to all resources"
   type        = map(string)
   default     = {}
+}
+
+variable "bucket" {
+  description = "Name of s3 bucket"
+  type        = string
 }
