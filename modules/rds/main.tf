928437eb495df046bc36e56e1d09f22ec823ff95
Adrian Meraz <adrian@rydeas.com>
2024-01-29 14:48:45 -0500

    only dashes allowed in rds ids

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -2,8 +2,8 @@ resource "aws_db_instance" "rds" {
   allocated_storage         = var.allocated_storage
   engine                    = var.engine
   engine_version            = var.engine_version
-  final_snapshot_identifier = "${var.identifier}_${var.environment}_final"
-  identifier                = "${var.identifier}_${var.environment}_db"
+  final_snapshot_identifier = "${var.identifier}-${var.environment}-final"
+  identifier                = "${var.identifier}-${var.environment}-db"
   instance_class            = var.instance_class
   password                  = var.password
   publicly_accessible       = var.publicly_accessible
