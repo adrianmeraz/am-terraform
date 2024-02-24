98db90b780cbaa90395bf2007870d383d49d1113
Adrian Meraz <adrian@rydeas.com>
2024-02-24 12:40:16 -0500

    standardized log fgroup name format

--- modules/logs/main.tf
+++ modules/logs/main.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_log_group" "main" {
-  name              = var.name
+  name              = "/${var.app_name}/${var.environment}/${var.service_name}"
   retention_in_days = var.retention_in_days
 
   tags = var.tags
