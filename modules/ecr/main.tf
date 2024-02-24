9b7e52a3951e87e73054e2296ed578aeec78cc65
Adrian Meraz <adrian@rydeas.com>
2024-02-24 12:46:30 -0500

    formatting

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -1,9 +1,8 @@
 data "aws_ecr_authorization_token" "token" {}
 
 resource  "aws_ecr_repository" "main" {
-  name = "${var.name_prefix}-ecr"
-
-  force_delete = var.force_delete
+  name                 = "${var.name_prefix}-ecr"
+  force_delete         = var.force_delete
   image_tag_mutability = var.image_tag_mutability
   
   image_scanning_configuration {
