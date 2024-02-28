67ef3944831574b377a6aedce5adb39ec7db58de
Adrian Meraz <adrian@rydeas.com>
2024-02-27 22:32:56 -0500

    formatting

--- apps/expatmagic/environments/dev/main.tf
+++ apps/expatmagic/environments/dev/main.tf
@@ -5,12 +5,12 @@ locals {
 module "app_spring_boot" {
   source = "../../../_templates/app_spring_boot"
 
-  app_name                       = local.app_name
-  aws_access_key                 = var.aws_access_key
-  aws_region                     = var.aws_region
-  aws_secret_key                 = var.aws_access_key
-  environment                    = local.environment
-  secret_map                     = var.secret_map
+  app_name       = local.app_name
+  aws_access_key = var.aws_access_key
+  aws_region     = var.aws_region
+  aws_secret_key = var.aws_access_key
+  environment    = local.environment
+  secret_map     = var.secret_map
   ecs = {
     launch_type: "FARGATE"
     memory_mb:   512
