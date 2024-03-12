ce9665d47bc0a24f53962db77bc6aab35c65b912
Adrian Meraz <adrian@rydeas.com>
2024-03-12 10:36:48 -0500

    now only using image hash without sha256 prefix

--- apps/_templates/app_python_serverless/main.tf
+++ apps/_templates/app_python_serverless/main.tf
@@ -89,7 +89,7 @@ module "lambdas" {
   memory_size          = local.lambda.memory_size_mb
   package_type         = "Image"
   role_arn             = module.iam_lambda_dynamo.role_arn
-  source_code_hash     = data.aws_ecr_image.latest.image_digest
+  source_code_hash     = split(":", data.aws_ecr_image.latest.image_digest)[1]
 
   tags                 = local.default_tags
   depends_on = [
