b9decb7eaf183e967227c4c237ed95904e56dc80
Adrian <adrian@rydeas.com>
2024-06-01 22:08:04 -0500

    removed redundant options

--- modules/apigw_lambda_integration/main.tf
+++ modules/apigw_lambda_integration/main.tf
@@ -1,7 +1,7 @@
 locals {
   cors_integration_response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-    "method.response.header.Access-Control-Allow-Methods" = "'DELETE,GET,OPTIONS,POST,PUT'",
+    "method.response.header.Access-Control-Allow-Methods" = "'DELETE,GET,POST,PUT'",
     "method.response.header.Access-Control-Allow-Origin"  = "'*'",
     "method.response.header.Access-Control-Allow-Credentials"  = "'true'",
   }
