2f30fa1b4dadb7b6940c5e2ad0202fde9c0be6fc
Adrian <adrian@rydeas.com>
2024-07-12 21:24:11 -0500

    changed attr name ExpiresAt

--- modules/dynamo_db/main.tf
+++ modules/dynamo_db/main.tf
@@ -7,7 +7,7 @@ resource "aws_dynamodb_table" "main" {
   stream_view_type = "NEW_AND_OLD_IMAGES"
   ttl {
     enabled = true
-    attribute_name = "EXPIRES_AT"
+    attribute_name = "ExpiresAt"
   }
 
   attribute {
