a14a08be108360f3be63f99d286d2db199811256
Adrian <adrian@rydeas.com>
2024-07-21 23:36:38 -0500

    added lambda

--- apps/expatmagic/co-checkmig/environments/dev/main.tf
+++ apps/expatmagic/co-checkmig/environments/dev/main.tf
@@ -72,6 +72,14 @@ module "app_python_serverless" {
       is_protected         = false
       lambda_environment   = var.lambda_environment
       timeout_seconds      = 10
+    },
+    {
+      base_function_name   = "get-travel-tokens"
+      http_method          = "GET"
+      image_config_command = "${local.lambda_cmd_prefix}.api_get_travel_tokens.lambda_handler"
+      is_protected         = false
+      lambda_environment   = var.lambda_environment
+      timeout_seconds      = 10
     }
   ]
 }
