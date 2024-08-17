b3d947704c52a63093ad6d4c8ff7b8d63b5cdd3b
Adrian <adrian@rydeas.com>
2024-08-16 19:33:43 -0500

    added lower to fdqn and ignore zone id

--- modules/route53_custom_domain/main.tf
+++ modules/route53_custom_domain/main.tf
@@ -47,7 +47,7 @@ resource "aws_acm_certificate_validation" "main" {
   # provider = aws.acm
 
   certificate_arn         = aws_acm_certificate.main.arn
-  validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.validation : lower(record.fqdn)]
 }
 
 ### Create Route 53 Records
