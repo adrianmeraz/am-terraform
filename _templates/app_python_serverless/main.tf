33c3476ed9cdb7041a99b6ae9b70b5bf90caa635
Adrian <adrian@rydeas.com>
2024-10-12 08:00:43 -0500

    moved shared secrets to ssm

--- _templates/app_python_serverless/main.tf
+++ _templates/app_python_serverless/main.tf
@@ -66,12 +66,12 @@ module "shared_secrets" {
   environment = var.environment
 }
 
-module "shared_secrets" {
-  source = "../../modules/ssm"
-
-  app_name    = var.shared_app_name
-  environment = var.environment
-}
+# module "shared_secrets" {
+#   source = "../../modules/ssm"
+#
+#   app_name    = var.shared_app_name
+#   environment = var.environment
+# }
 
 # Secrets only created and stored the first run
 
--- modules/shared_secrets/main.tf
+++ modules/shared_secrets/main.tf
@@ -1,9 +1,13 @@
-data "aws_secretsmanager_secret_version" "shared" {
-  secret_id = "shared/${var.app_name}/${var.environment}/secret"
+# data "aws_secretsmanager_secret_version" "shared" {
+#   secret_id = "shared/${var.app_name}/${var.environment}/secret"
+# }
+
+data "aws_ssm_parameter" "shared" {
+  name = "/shared/${var.app_name}/${var.environment}/secret"
 }
 
 locals  {
-  shared_secret_map  = merge(jsondecode(data.aws_secretsmanager_secret_version.shared.secret_string))
+  shared_secret_map  = jsondecode(data.aws_ssm_parameter.shared.value)
   cognito_pool_name  = local.shared_secret_map["AWS_COGNITO_POOL_NAME"]
   cognito_pool_arn   = tolist(data.aws_cognito_user_pools.shared.arns)[0]
   cognito_pool_id    = tolist(data.aws_cognito_user_pools.shared.ids)[0]
