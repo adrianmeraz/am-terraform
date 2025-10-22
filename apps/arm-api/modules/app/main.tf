cf079d604c588c709086980011302df0f3f88912
JackTheRooster <adrian@rydeas.com>
2025-10-21 23:25:22 -0500

    added shared app name env var

--- apps/arm-api/modules/app/main.tf
+++ apps/arm-api/modules/app/main.tf
@@ -7,7 +7,7 @@ locals {
       http_method     = "ANY"
       module_name     = "main"
       path_part       = "{proxy+}"
-      is_authorized    = false
+      is_authorized   = false
       timeout_seconds = local.lambda_timeout_seconds
     },
   ]
